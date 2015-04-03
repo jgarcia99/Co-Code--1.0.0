dda2c033936c8ef74f2643552cc48a6c5e7f1e2a
Josuel C Garcia <garciajosuel@yahoo.com>
2015-04-03 15:33:46 -0400

    image carousels complete

--- src/app/locations/locations.controller.js
+++ src/app/locations/locations.controller.js
@@ -9,7 +9,7 @@ angular.module('coCode')
 
     $scope.slides = [
         {
-            image: 'https://orlando-life.com/sites/default/files/Stardust_20120402_0009.jpg'
+            image: 'http://a4.urbancdn.com/w/s/Qo/4FVmu6amvhEjZF-640m.jpg'
         },
         {
             image: 'http://static1.squarespace.com/static/53050553e4b07e4708f6858c/t/5318b4dfe4b0fa5080a9f8aa/1394128095934/4613916-Stardust_Coffee_and_Video_Orlando.jpg'
@@ -18,16 +18,16 @@ angular.module('coCode')
             image: 'http://francisbcookin.com/content/images/2014/Jul/DSC_0192-1.JPG'
         },
         {
-            image: 'http://a4.urbancdn.com/w/s/Qo/4FVmu6amvhEjZF-640m.jpg'
+            image: 'https://orlando-life.com/sites/default/files/Stardust_20120402_0009.jpg'
         }
     ];
 
     $scope.slides2 = [
         {
-            image: 'http://amplifiedculture.com/wp-content/uploads/2011/02/Austins_Coffee-1-e1301102123170.jpg'
+            image: 'http://amplifiedculture.com/wp-content/uploads/2011/02/Austins_Coffee-6-e1301101795213.jpg'
         },
         {
-            image: 'http://amplifiedculture.com/wp-content/uploads/2011/02/Austins_Coffee-6-e1301101795213.jpg'
+            image: 'http://amplifiedculture.com/wp-content/uploads/2011/02/Austins_Coffee-1-e1301102123170.jpg'
         },
         {
             image: 'http://rivista-cdn.orlandomagazine.com/Orlando-Magazine/April-2014/A-Way-With-Words/Open-Words-099.jpg?ver=1395082914'
@@ -54,20 +54,33 @@ angular.module('coCode')
 
     $scope.slides4 = [
         {
-            image: 'http://mealsandmiles.com/wp-content/uploads/2011/03/IMG_5009.jpg'
+            image: 'http://vp.cdn.cityvoterinc.com/g/00-00-00-49-10-21-491021_388557.jpg'
         },
         {
             image: 'http://www.orlandosentinel.com/media/photo/2009-07/48352153.jpg'
         },
         {
-            image: 'http://vp.cdn.cityvoterinc.com/g/00-00-00-49-10-21-491021_388557.jpg'
+            image: 'http://mealsandmiles.com/wp-content/uploads/2011/03/IMG_5009.jpg'
         },
         {
             image: 'http://www.orlandoweekly.com/binary/0431/1472263.jpg'
         }
     ];
 
-
+    $scope.slides5 = [
+        {
+            image: 'http://i.ytimg.com/vi/okTdqGGQKW8/hqdefault.jpg'
+        },
+        {
+            image: 'http://a3.urbancdn.com/w/s/Ov/WJO4sDIR4Nai3O-640m.jpg'
+        },
+        {
+            image: 'http://www.abejadecafe.com/wp-content/uploads/2013/12/image6.jpg'
+        },
+        {
+            image: 'http://theorlandoscene.com/wp-content/uploads/2014/05/vespr4.jpg'
+        }
+    ];
 
     vm.obj = $firebaseArray(userInfo);
     vm.logOut = Auth.logout;
